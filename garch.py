bdfbe7b4a7835e62bcca88ed81008fd947054e5e
Tony Lee <chunglee2018@u.northwestern.edu>
2024-04-19 16:35:45 -0400

    udpate

--- garch.py
+++ garch.py
@@ -4,8 +4,7 @@ from typing import List
 import pickle
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt   
-from arch import arch_model
+import matplotlib.pyplot as plt
 from dojo.actions import BaseAction
 from dojo.agents import BaseAgent
 from dojo.environments.uniswapV3 import UniV3Obs, UniV3Trade
@@ -21,10 +20,14 @@ class GARCHModel:
         self.data_file = data_file
         self.model = loaded_model
         self.results = None
+        self.best_p = None
+        self.best_q = None
 
     def load_data(self):
+        max_data = 100
         # Load the CSV file into a DataFrame
-        self.data = pd.read_csv(self.data_file) 
+        self.data = pd.read_csv(self.data_file)
+        self.data = self.data[:max_data]
 
     def preprocess_data(self):
         # Extract the log returns data
@@ -37,29 +40,34 @@ class GARCHModel:
         # Main point of entry for workflow
         self.load_data()
         self.preprocess_data()
-        
+
         #Fitting Model and Saving it
         self.fit_model()
-        with open('/Users/andrew/Documents/Dojo_Test/Models/Garch_Model_BTCC.pkl' , 'wb') as f:
+        with open('/Users/tonylee/Documents/vanna/vanna-cexdata/models/garch.pkl' , 'wb') as f:
             pickle.dump(self.results, f)
             print(os.getcwd())
-    
 
         #Model Visualization
+        print("visualization")
         self.visualization()
 
     def fit_model(self):
+        print(self.data["log_returns"])
         if not self.model:
         #fitting model
             best_aic = 1000000000
             # grid-searching optimal model parameters
+
             for p in range(1, 5):
                 for q in range(1, 5):
+                    print("here")
                     self.model = arch.arch_model(self.data["log_returns"], vol='Garch', p=p, q=q)
                     results = self.model.fit(disp='off')
                     if results.aic < best_aic:
                         best_aic = results.aic
                         best_results = results
+                        self.best_p = p
+                        self.best_q = q
             self.results = best_results
             # Fit the model
             print(self.results.summary())
@@ -68,11 +76,15 @@ class GARCHModel:
         #Rolling Forecast
         rolling_predictions = []
         test_size = len(self.data["log_returns"])
-        for i in range(max(p, q)+1 , test_size + 1):
+        for i in range(max(self.best_p, self.best_q)+1 , test_size + 1):
+            # get log returns of data stored by csv
             train = self.data["log_returns"][:i]
+            # build initial garch model using trained data
+            self.model = arch.arch_model(train, vol='Garch', p=self.best_p, q=self.best_q)
             model_fit = self.model.fit(disp = "off")
             pred = model_fit.forecast(horizon = 1)
             rolling_predictions.append((i, np.sqrt(pred.variance.values[-1][0])))
+            print(i)
         self.rolling_predictions = pd.DataFrame(rolling_predictions, columns=['Index', 'Predicted_Variance'])
 
     def visualization(self):
@@ -92,12 +104,38 @@ class GARCHModel:
         plt.grid(True)
         plt.show()
 
+    def update_and_predict(self, new_mid_price):
+        # Updating the latest middle price
+        new_log_return = abs(np.log(new_mid_price / self.data['middle_price'].iloc[-1])) * 100000
+
+        # Append the new log return to the DataFrame
+        new_row = pd.DataFrame({'middle_price': [new_mid_price], 'log_returns': [new_log_return]})
+        self.data = pd.concat([self.data, new_row], ignore_index=True)
+
+        # Store the updated DataFrame to CSV
+        self.data.to_csv(self.data_file, index=False)
+
+        # Re-fit the model with updated data if necessary
+        if not self.model:
+            self.fit_model()
+        else:
+            # Update existing model with new data point
+            self.model = arch.arch_model(self.data['log_returns'], vol='Garch', p=self.best_p, q=self.best_q)
+            self.results = self.model.fit(last_obs=len(self.data) - 1, update_freq=5, disp='off')
+
+        # Predicting the next variance
+        pred = self.results.forecast(horizon=1)
+        predicted_variance = np.sqrt(pred.variance.values[-1][0])
+
+        return predicted_variance
+
 
 
 #creating model and testing
 def create_new_model(data_file):
     model = GARCHModel(data_file=data_file)
     model.run()
+    return model
 
 # loading a pre-trained GARCH model and testing
 def load_pretrained_model(data_file, model_file):
@@ -106,6 +144,6 @@ def load_pretrained_model(data_file, model_file):
     model = GARCHModel(data_file=data_file, loaded_model=loaded_model)
     model.run()
 
-
-
-
+data_file = 'datasets/coinbase_quotes_2023-01-01_BTC-USDT.csv'
+testmodel = create_new_model(data_file)
+print(testmodel.update_and_predict(30000))
