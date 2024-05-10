e0bb4fbb4819281b52e8a920c7128fb7fbc40eaf
Tony Lee <chunglee2018@u.northwestern.edu>
2024-05-10 14:22:20 -0400

    env setup

--- download_scripts/download_coinbase.py
+++ download_scripts/download_coinbase.py
@@ -1,14 +1,26 @@
 import argparse
 from tardis_dev import datasets
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 def main(from_date, to_date):
+    # Retrieve the API key from environment variables
+    api_key = os.getenv("TARDIS_API_KEY")
+
+    # Ensure the API key is available
+    if not api_key:
+        raise Exception("API key not found. Please check your .env file.")
+
     datasets.download(
         exchange="coinbase",
         data_types=["quotes"],
         from_date=from_date,
         to_date=to_date,
         symbols=["BTC-USDT", "ETH-USDT"],
-        api_key="TD.DV-W-SYHUhTufUsm.EN4btEiFDy9wZ9Y.2osoo9EgGWo6zqr.6-uNQiQoOf6FbaV.7cnPAozw0wnmqwY.O40t"
+        api_key=api_key
     )
 
 if __name__ == "__main__":
@@ -17,8 +29,4 @@ if __name__ == "__main__":
     parser.add_argument("--to_date", type=str, help="End date in YYYY-MM-DD format")
     args = parser.parse_args()
 
-    # Use command-line arguments if provided, else use default values
-    from_date = args.from_date
-    to_date = args.to_date
-
-    main(from_date, to_date)
+    main(args.from_date, args.to_date)
